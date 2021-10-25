9de432ddd844e0402d7eb9874f1d6ab89562681c
shane1595042264 <a1595042264@gmail.com>
2021-10-25 08:36:21 -0400

    debugging, update deprecated package

--- server/index.js
+++ server/index.js
@@ -20,8 +20,10 @@ app.use(express.urlencoded({ limit: '30mb', extended: true }))
 const CONNECTION_URL = 'mongodb+srv://douvleplus:Mark321654.@cluster0.fz0al.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }) //a promise
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }) //a promise
+//   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+//   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', true ); // make sure don't get any warning in the console
+// mongoose.set('useFindAndModify', true ); // make sure don't get any warning in the console
+
+mongoose.connect(CONNECTION_URL).then(()=>{console.log(`Server Running on Port: http://localhost:${PORT}`)}).catch((error) => console.log(`${error} did not connect`));
